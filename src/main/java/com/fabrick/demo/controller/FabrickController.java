f419b7c2ea87b2dd6cf332b8711e994835868e60
federico.conoci <federico.conoci@webscience.it>
2021-06-18 15:47:48 +0200

    Refactoring

--- src/main/java/com/fabrick/demo/controller/FabrickController.java
+++ src/main/java/com/fabrick/demo/controller/FabrickController.java
@@ -5,6 +5,8 @@ import com.fabrick.demo.controller.dto.TransactionsDTO;
 import com.fabrick.demo.controller.dto.TransferDTO;
 import com.fabrick.demo.service.FabrickService;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping()
+@Validated
 public class FabrickController {
 
     private final FabrickService service;
@@ -22,20 +25,26 @@ public class FabrickController {
     }
 
     @GetMapping("/balance")
-    public double getBalance(@RequestParam String accountId){
-        return service.getBalanceFromAPI(accountId);
+    public ResponseEntity<Double> getBalance(@RequestParam String accountId) {
+        if(valid(accountId)){
+            return ResponseEntity.ok(service.getBalanceFromAPI(accountId));
+        }
+        return ResponseEntity.badRequest().build();
     }
 
     @GetMapping("/transactions")
-    public List<TransactionsDTO.Transaction> getBalance(@RequestParam
-                                                        String accountId,
-                                                        @RequestParam
-                                                        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-                                                        LocalDate from,
-                                                        @RequestParam
-                                                        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-                                                        LocalDate to){
-        return service.getTransactionGivenAPeriod(accountId, from, to);
+    public ResponseEntity<Collection<TransactionsDTO.Transaction>> getTransactions(@RequestParam
+                                                                     String accountId,
+                                                             @RequestParam
+                                                             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                                                     LocalDate from,
+                                                             @RequestParam
+                                                             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                                                     LocalDate to) {
+        if(valid(accountId) && validDate(from) && validDate(to)){
+            return ResponseEntity.ok(service.getTransactionGivenAPeriod(accountId, from, to));
+        }
+        return ResponseEntity.badRequest().build();
     }
 
     @PostMapping("/transfer")
@@ -43,5 +52,18 @@ public class FabrickController {
         return service.doTransfer(transferDTO);
     }
 
+    private boolean valid(String accountId) {
+        if (accountId == null || accountId.length() == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validDate(LocalDate date) {
+        if (date.isAfter(LocalDate.now())) {
+            return false;
+        }
+        return true;
+    }
 
 }
