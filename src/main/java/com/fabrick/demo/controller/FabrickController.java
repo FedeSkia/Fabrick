e86d01ea457bb9d1722ca670764a283607ae51a8
federico.conoci <federico.conoci@webscience.it>
2021-06-19 11:40:25 +0200

    mocked api client for balance

--- src/main/java/com/fabrick/demo/controller/FabrickController.java
+++ src/main/java/com/fabrick/demo/controller/FabrickController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.List;
 
 @RestController
 @RequestMapping()
--- src/test/java/com/fabrick/demo/controller/FabrickControllerAccountBalanceTest.java
+++ src/test/java/com/fabrick/demo/controller/FabrickControllerAccountBalanceTest.java
@@ -1,12 +1,9 @@
 package com.fabrick.demo.controller;
 
-import com.fabrick.demo.MockedResponses;
 import com.fabrick.demo.client.ApiClient;
 import com.fabrick.demo.controller.dto.AccountBalanceDTO;
-import com.fabrick.demo.controller.dto.TransactionsDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,14 +11,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FabrickControllerAccountBalanceTest {
@@ -30,23 +24,40 @@ class FabrickControllerAccountBalanceTest {
     Integer port;
     @Autowired
     private TestRestTemplate testRestTemplate;
-
+    @MockBean
+    private ApiClient apiClient;
     String validAccount = "14537780";
 
 
     @Test
     public void check_balance_is_retrieved() {
-
+        AccountBalanceDTO accountBalanceDTO = mockAPIClientForBalance();
         ResponseEntity<Double> response = testRestTemplate.getForEntity("http://localhost:" + port + "/balance?accountId=" + validAccount, Double.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isEqualTo(49441.71);
+        assertThat(response.getBody()).isEqualTo(100);
     }
 
     @Test
-    public void check_balance_when_invalid_account() throws IOException {
+    public void check_balance_when_invalid_account() {
         String invalidAccountId = "xyz";
         ResponseEntity<Double> response = testRestTemplate.getForEntity("http://localhost:" + port + "/balance?accountId=" + invalidAccountId, Double.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    private AccountBalanceDTO mockAPIClientForBalance(){
+        AccountBalanceDTO accountBalanceDTO = AccountBalanceDTO.builder()
+                .error(null)
+                .status("OK")
+                .payload(AccountBalanceDTO.Payload.builder()
+                        .balance(100)
+                        .availableBalance(100)
+                        .currency("EUR")
+                        .build())
+                .build();
+
+        Mockito.when(apiClient.getBalanceFromFabrickAPI(ArgumentMatchers.anyString()))
+                .thenReturn(Optional.of(accountBalanceDTO));
+        return accountBalanceDTO;
     }
 
 
